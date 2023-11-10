0fc310311da938d40919e9093171d69a94e5a687
fransoufil <fran.sf@uol.com.br>
2023-11-10 14:43:14 -0300

    Main e Calculator com membros estaticos

---
+++ src/main/Program.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculator;
+
+public class Program {
+	
+	public static double PI = 3.14159;
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = Calculator.circumference(radius);
+		
+		double v = Calculator.volume(radius);
+		
+		System.out.printf("Circumference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", PI);
+		
+		sc.close();
+	}
+
+}
